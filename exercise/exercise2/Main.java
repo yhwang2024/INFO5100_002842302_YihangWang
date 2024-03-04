4e384925a7ce3fd030ccacf13d731fc7b863284c
Yihang Wang <158772371+yhwang2024@users.noreply.github.com>
2024-03-03 23:44:19 -0800

    Update Main.java

--- exercise/exercise2/Main.java
+++ exercise/exercise2/Main.java
@@ -1,1 +1,132 @@
-import
+abstract class Shape {
+    protected String color;
+
+    // Constructor
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    // Abstract methods
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+
+    // Static method to print shape type
+    public static void printShapeType(Shape shape) {
+        System.out.println("This is a " + shape.getClass().getSimpleName());
+    }
+}
+
+class Triangle extends Shape {
+    private double side1, side2, side3;
+
+    // Constructor
+    public Triangle(String color, double side1, double side2, double side3) {
+        super(color);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    // Overriding abstract methods
+    @Override
+    public double calculateArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return side1 + side2 + side3;
+    }
+}
+
+class Rectangle extends Shape {
+    private double length, width;
+
+    // Constructor
+    public Rectangle(String color, double length, double width) {
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+
+    // Overriding abstract methods
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    // Constructor
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    // Overriding abstract methods
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Square extends Shape {
+    private double side;
+
+    // Constructor
+    public Square(String color, double side) {
+        super(color);
+        this.side = side;
+    }
+
+    // Overriding abstract methods
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 4 * side;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape triangle = new Triangle("Red", 3, 4, 5);
+        Shape rectangle = new Rectangle("Blue", 4, 5);
+        Shape circle = new Circle("Green", 3);
+        Shape square = new Square("Yellow", 4);
+
+        System.out.println("Triangle Area: " + triangle.calculateArea());
+        System.out.println("Triangle Perimeter: " + triangle.calculatePerimeter());
+
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.calculatePerimeter());
+
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Circle Perimeter: " + circle.calculatePerimeter());
+
+        System.out.println("Square Area: " + square.calculateArea());
+        System.out.println("Square Perimeter: " + square.calculatePerimeter());
+
+        // Demonstrating polymorphism
+        Shape[] shapes = {triangle, rectangle, circle, square};
+        for (Shape shape : shapes) {
+            Shape.printShapeType(shape);
+        }
+    }
+}
